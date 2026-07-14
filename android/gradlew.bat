ed39af8cd46252d6f9974f1a8ff182938425f49e
Eetu Kaunismäki <eetu.kaunismaki@punosmobile.com>
2026-07-14 15:33:34 +0300

    rn upgrade to 0.86.0, tested android side

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,3 +1,7 @@
+@REM Copyright (c) Meta Platforms, Inc. and affiliates.
+@REM
+@REM This source code is licensed under the MIT license found in the
+@REM LICENSE file in the root directory of this source tree.
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
@@ -52,10 +56,8 @@ echo location of your Java installation. 1>&2
 goto fail
 :execute
 @rem Setup the command line
-set CLASSPATH=
 @rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
-
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
 :end
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
